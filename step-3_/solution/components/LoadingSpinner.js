eb85565c91de59b1e5be1a2e5202427b0cc9d51d
santiago-elustondo <elus2012@gmail.com>
2019-02-26 10:11:38 -0500

    r

--- step-3_/solution/components/LoadingSpinner.js
+++ step-3_/solution/components/LoadingSpinner.js
@@ -4,7 +4,6 @@ import { Platform, ProgressBarAndroid, ProgressViewIOS } from 'react-native'
 export class LoadingSpinner extends React.Component { 
   render() {
     const { style } = this.props
-
     return Platform.OS === 'ios' ? (
       <ProgressViewIOS style={style}/>
     ) : (
